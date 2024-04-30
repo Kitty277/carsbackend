968425447f829eded23eea6c65b1e453cbe6f6f0
Abhiram Gorle <80030901+abhiramgorle@users.noreply.github.com>
2024-04-30 22:47:58 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,24 @@
 const express = require('express')
+const cors = require('cors');
 const app = express()
 const port = process.env.PORT || 5000
 const dbConnection = require('./db')
 app.use(express.json())
+
+var corsOptions = {
+  origin: ['http://localhost:3000',"https://carsfrontend.vercel.app"],
+  optionsSuccessStatus: 200, // For legacy browser support
+  credentials: true,
+};
+app.use(cors(corsOptions));
+
+app.use(function (request, response) {
+  response.header("Access-Control-Allow-Origin", "*");
+  response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+});
+
+
+
 
 app.use('/api/cars/' , require('./routes/carsRoute'))
 app.use('/api/users/' , require('./routes/usersRoute'))
